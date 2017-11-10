6a0ac07a7ec10a12e2bec9fb8ca0ff0eb7f43dca
Justin Dietz <justin.r.dietz@gmail.com>
2017-11-10 10:23:14 -0800

    Testing CI

---
+++ NewFile/index.js
@@ -0,0 +1,23 @@
+module.exports = function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+    context.log(req.body);
+    if (req.body && req.body.challenge) {
+        context.log('testing making a change');
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: req.body.challenge
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "No Challenge Sent"
+        };
+    }
+
+
+
+    context.done();
+};
+
+//{"challenge":"3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P"}
