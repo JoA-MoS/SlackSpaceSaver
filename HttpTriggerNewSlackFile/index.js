f533a3f49400aca9360d4834597c49242b47718a
Justin Dietz <justin.r.dietz@gmail.com>
2017-11-10 15:27:01 -0800

    removed route

--- HttpTriggerNewSlackFile/index.js
+++ HttpTriggerNewSlackFile/index.js
@@ -3,7 +3,10 @@ const express = require("express");
 
 // Create express app as usual
 const app = express();
-app.get("/api/file", (req, res) => res.json({ request: req }));
+
+
+
+app.get("/", (req, res) => res.json({ hello: "world" }));
 //app.get("/api/:foo/:bar", (req, res) => res.json({ foo: req.params.foo, bar: req.params.bar }));
 
 // Binds the express app to an Azure Function handler
