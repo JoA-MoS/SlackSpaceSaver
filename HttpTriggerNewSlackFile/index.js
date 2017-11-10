b6ea343221eee742664d711aa95f141d27b14d73
Justin Dietz <justin.r.dietz@gmail.com>
2017-11-10 15:46:37 -0800

    Going back to not trying to implement slack node package

--- HttpTriggerNewSlackFile/index.js
+++ HttpTriggerNewSlackFile/index.js
@@ -1,13 +1,28 @@
-const createAzureFunctionHandler = require("azure-function-express").createAzureFunctionHandler;
-const express = require("express");
+
+'use strict';
+
+const createSlackEventAdapter = require('@slack/events-api').createSlackEventAdapter;
+const slackEvents = createSlackEventAdapter(process.env.SLACK_VERIFICATION_TOKEN);
+
+const createAzureFunctionHandler = require('azure-function-express').createAzureFunctionHandler;
+const express = require('express');
+const bodyParser = require('body-parser');
 
 // Create express app as usual
 const app = express();
 
+app.use(bodyParser.json());
+app.use('/slack/events', slackEvents.expressMiddleware());
+
+slackEvents.on('message', (event) => {
+    console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+});
+
+slackEvents.on('error', console.error);
 
 
-app.get("/api/*", (req, res) => res.json({ hello: "world" }));
-//app.get("/api/:foo/:bar", (req, res) => res.json({ foo: req.params.foo, bar: req.params.bar }));
+// app.get('/api/*', (req, res) => res.json({ hello: 'world' }));
+// //app.get("/api/:foo/:bar", (req, res) => res.json({ foo: req.params.foo, bar: req.params.bar }));
 
 // Binds the express app to an Azure Function handler
 module.exports = createAzureFunctionHandler(app);
