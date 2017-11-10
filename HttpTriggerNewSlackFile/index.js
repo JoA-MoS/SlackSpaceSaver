f47d6892537597378d0e13c532639fd97c8279a8
Justin Dietz <justin.r.dietz@gmail.com>
2017-11-10 15:09:04 -0800

    all routes

--- HttpTriggerNewSlackFile/index.js
+++ HttpTriggerNewSlackFile/index.js
@@ -3,13 +3,8 @@ const express = require("express");
 
 // Create express app as usual
 const app = express();
-app.post("/api/:foo/:bar", (req, res) => {
-    req.context.log({ Log: "I ran" });
-    res.json({
-        foo: req.params.foo,
-        bar: req.params.bar
-    });
-});
+app.get("/api/:foo", (req, res) => res.json({ foo: req.params.foo }));
+app.get("/api/:foo/:bar", (req, res) => res.json({ foo: req.params.foo, bar: req.params.bar }));
 
 // Binds the express app to an Azure Function handler
 module.exports = createAzureFunctionHandler(app);
