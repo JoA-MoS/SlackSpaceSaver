e9de0ec845a15c55d81eef87ec36530f5f2876b7
Justin Dietz <justin.r.dietz@gmail.com>
2017-11-10 11:35:45 -0800

    cleaning up the logging and adding the file change and file created

--- HttpTriggerNewSlackFile/index.js
+++ HttpTriggerNewSlackFile/index.js
@@ -1,7 +1,7 @@
 module.exports = function (context, req) {
     context.log('================== New Req ======================');
-    context.log(context);
-    context.log(req);
+    // context.log(context);
+    // context.log(req);
     // if req has a body process body
     if (req.body) {
         // if a challenge request respond with challenge
@@ -14,6 +14,16 @@ module.exports = function (context, req) {
                             'Content-Type': 'application/json'
                         },
                         body: { challenge: req.body.challenge }
+
+                    };
+                    break;
+                case 'file_created':
+                    context.res = {
+                        // status: 200, /* Defaults to 200 */
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: { incoming_req: req.body }
 
                     };
                     break;
@@ -34,7 +44,7 @@ module.exports = function (context, req) {
                         headers: {
                             'Content-Type': 'application/json'
                         },
-                        body: { error: context.req.type + 'is not implemented' }
+                        body: { error: context.req.type + ' is not implemented' }
 
                     };
                     break;
