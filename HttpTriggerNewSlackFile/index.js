d5d42f3bc2acee40899fa47d7789a0eb95b8e022
Justin Dietz <justin.r.dietz@gmail.com>
2017-11-10 15:30:49 -0800

    figuring out the routes

--- HttpTriggerNewSlackFile/index.js
+++ HttpTriggerNewSlackFile/index.js
@@ -6,7 +6,7 @@ const app = express();
 
 
 
-app.get("/", (req, res) => res.json({ hello: "world" }));
+app.get("/api/*", (req, res) => res.json({ hello: "world" }));
 //app.get("/api/:foo/:bar", (req, res) => res.json({ foo: req.params.foo, bar: req.params.bar }));
 
 // Binds the express app to an Azure Function handler
