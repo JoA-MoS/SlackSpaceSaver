998ba30f5d1d806b56da9a80268455d2b4c44800
Justin Dietz <justin.r.dietz@gmail.com>
2017-11-10 15:17:37 -0800

    Static Route

--- HttpTriggerNewSlackFile/index.js
+++ HttpTriggerNewSlackFile/index.js
@@ -3,8 +3,8 @@ const express = require("express");
 
 // Create express app as usual
 const app = express();
-app.get("/api/:foo", (req, res) => res.json({ foo: req.params.foo }));
-app.get("/api/:foo/:bar", (req, res) => res.json({ foo: req.params.foo, bar: req.params.bar }));
+app.get("/api/file", (req, res) => res.json({ request: req }));
+//app.get("/api/:foo/:bar", (req, res) => res.json({ foo: req.params.foo, bar: req.params.bar }));
 
 // Binds the express app to an Azure Function handler
 module.exports = createAzureFunctionHandler(app);
