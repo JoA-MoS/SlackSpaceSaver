5ea609a5b25adfc88631354356787c6621a85c9e
Justin Dietz <justin.r.dietz@gmail.com>
2017-11-10 14:59:39 -0800

    switch to post

--- HttpTriggerNewSlackFile/index.js
+++ HttpTriggerNewSlackFile/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 // Create express app as usual
 const app = express();
-app.get("/api/:foo/:bar", (req, res) => {
+app.post("/api/:foo/:bar", (req, res) => {
     res.json({
         foo: req.params.foo,
         bar: req.params.bar
